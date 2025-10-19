aaf9e38ea5ef07cd9b16f88df5349c2d3ae3451b
Mahesh <maheshpererakm@gmail.com>
2025-10-19 06:18:15 +0530

    commit messsage

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import "./App.css";
 import "./index.css";
 import HeaderContent from "./Components/HeaderContent/HeaderContent";
 import BodyContent from "./Components/BodyContent/BodyContent";
+import Home from "./Pages/Home";
 
 function App() {
   return (
     <>
       <HeaderContent />
-      <BodyContent>
+      <Home />
+      {/* <BodyContent>
         <h1>Home Page</h1>
         <p>
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore
@@ -16,7 +18,7 @@ function App() {
           exercitationem. In, id! Nihil sunt quisquam tenetur deserunt voluptas
           eligendi laborum fugit in. Deleniti!
         </p>
-      </BodyContent>
+      </BodyContent> */}
     </>
   );
 }
---
+++ src/Pages/Home.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Home() {
+  return (
+    <div>
+      <h1>Home Page</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore tenetur
+        explicabo sunt maiores excepturi consectetur ad exercitationem. In, id!
+        Nihil sunt quisquam tenetur deserunt voluptas eligendi laborum fugit in.
+        Deleniti!
+      </p>
+    </div>
+  );
+}
+
+export default Home;
